1979d14e52a6cea275bfdf5c8f7c0058014cac77
Gregory ARTHUS <garthus@adminux.fr>
2021-03-10 16:39:34 +0100

    debug(): betty compliant

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -73,11 +73,11 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (tree)
 	{
 		if (tree->left == NULL && tree->right == NULL)
-            	return (1);
-        if ((binary_tree_8(tree->left) == binary_tree_8(tree->right)) &&
-            binary_tree_is_perfect(tree->left) &&
-            binary_tree_is_perfect(tree->right))
-        return (1);
+			return (1);
+		if ((binary_tree_8(tree->left) == binary_tree_8(tree->right)) &&
+				binary_tree_is_perfect(tree->left) &&
+				binary_tree_is_perfect(tree->right))
+			return (1);
 	}
 	return (0);
 }
