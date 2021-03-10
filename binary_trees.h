aae5241957ce50aa688e86a4b879b3a765e64817
Gregory ARTHUS <garthus@adminux.fr>
2021-03-10 16:57:38 +0100

    update(): header file

--- binary_trees.h
+++ binary_trees.h
@@ -47,4 +47,5 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *f,
 const binary_tree_t *s);
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 #endif
